fc1445cda8163c1eef5f34c37c23272fa66db590
Domae-back-end <ty_ty123@naver.com>
2023-07-21 21:54:40 +0900

    feat : Spring 프로젝트 생성
    # Spring Web, Lombok 라이브러리 추가

---
+++ src/test/java/com/twitter/clone/twitterclone/TwitterCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twitter.clone.twitterclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
